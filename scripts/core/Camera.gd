92b8c5233fe665cc0bd627796c8489794ec7b674
ShadowRaven <shadowraven2025@gmail.com>
2026-03-27 14:22:49 +0300

    Fix Camera2D offset redefinition

--- scripts/core/Camera.gd
+++ scripts/core/Camera.gd
@@ -2,9 +2,9 @@ extends Camera2D
 
 @export var follow_speed: float = 5.0
 @export var target: Node2D = null
-@export var offset: Vector2 = Vector2.ZERO
+@export var camera_offset: Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
 	if target:
-		var target_position = target.global_position + offset
+		var target_position = target.global_position + camera_offset
 		global_position = global_position.lerp(target_position, follow_speed * delta)
