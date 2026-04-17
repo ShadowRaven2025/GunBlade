03a68dac378bc858648e7410e04f29f9aeb82d3b
ShadowRaven <shadowraven2025@gmail.com>
2026-04-17 14:08:19 +0300

    Remove stray Python files

--- Untitled-1.py
+++
@@ -1,5 +0,0 @@
-a=int(input('Ввидите число:'))
-if a%2==0 and a>0:
-    print('Да')
-else:
-    print('Нет')
--- Untitled-2.py
+++
@@ -1,9 +0,0 @@
-x=float(input())
-y=float(input())
-if x>0 and y>0 or x<0 and y<0:
-    print('Да')
-else:
-    print('Нет')
-
-
-
